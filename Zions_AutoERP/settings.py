c118919b869259a42d894efcb0f0e1b8e02c642f
Alejandro <ThinkRamen@gmail.com>
2024-05-03 12:08:51 -0700

    added allowed hosts

--- Zions_AutoERP/settings.py
+++ Zions_AutoERP/settings.py
@@ -31,7 +31,7 @@ IP = (
     else os.environ.get("PROD_IP")
 )
 print(IP)
-ALLOWED_HOSTS = ["localhost", f"{IP}:8000", IP]
+ALLOWED_HOSTS = ["localhost", "10.0.0.93", "10.0.0.93:8000"]
 
 
 # Application definition

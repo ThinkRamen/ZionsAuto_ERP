e6eef5fe71bf3ecb47ebb67baa138e2ee06294e8
Alejandro <ThinkRamen@gmail.com>
2024-05-03 11:52:20 -0700

    gunicorn_config

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -1,4 +1,4 @@
 command = "/home/ramos/Desktop/dev/ZionsAuto_ERP/.venv/bin/gunicorn"
 pythonpath = "/home/ramos/Desktop/dev/ZionsAuto_ERP"
-bind = "10.0.0.93:8000"
+bind = "/home/ramos/Desktop/dev/ZionsAuto_ERP/ZionsAuto_ERP.sock"
 workers = 3

3ac90f495f479ab66103e56075e72ffbc55bd8f5
Alejandro <ThinkRamen@gmail.com>
2024-05-03 14:32:15 -0700

    static files

--- Zions_AutoERP/settings.py
+++ Zions_AutoERP/settings.py
@@ -136,7 +136,8 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles/")]
-STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
